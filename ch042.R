2bd794169b7bfde9b9c59f10e446ddd3a3ee5c73
mightycupid <akang5@siu.edu>
2015-07-18 11:44:06 -0500

    LAB04B

---
+++ ch042.R
@@ -0,0 +1,45 @@
+x <- 2
+x
+y = 5
+y
+
+z <- 3
+z
+3 <- z
+Z
+
+a <- b <- 7
+a
+b
+
+assign("j", 4)
+j
+
+x123 <- 100
+x123
+
+x_value <- 200
+x_value
+	
+x.value <- 300
+x.value
+
+x__value <- 400
+x__value
+x..value <- 500
+x..value
+
+1x <- 400
+_value <- 100
+
+j <- 100
+j
+rm(j)
+j <- 200
+j
+gc(j)
+
+x1 <- 17
+x1
+X1
+
